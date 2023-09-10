b258c230393e05bf11c1916f04a1664628ea8ef4
brionesl <143744654+LeonelAkio@users.noreply.github.com>
2023-09-10 21:38:03 +0000

    Le premier projet

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def count_word(chaine):
+        mots = chaine.split()
+        nb_de_mots = len(mots)
+        return nb_de_mots
+
+
+# Demander à l'utilisateur de saisir une chaîne 
+chaine = input("Entrez une chaine de caractères : \n")
+nb_de_mots = count_word(chaine)
+print("Le nombre de mots dans la chaîne est : \n", nb_de_mots)
 
