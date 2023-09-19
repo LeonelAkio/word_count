e393a3cf590a640f38bf26ce718f7be3baf1fd23
brionesl <143744654+LeonelAkio@users.noreply.github.com>
2023-09-19 15:28:01 +0000

    ajout de commentaires

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#créé par leonel akio briones
+
+#définit la fonction count_word
 def count_word(chaine):
         mots = chaine.split()
         nb_de_mots = len(mots)
@@ -6,6 +9,8 @@ def count_word(chaine):
 
 # Demander à l'utilisateur de saisir une chaîne 
 chaine = input("Entrez une chaine de caractères : \n")
+#définit le nombre de mots
 nb_de_mots = count_word(chaine)
+#retourne le nombre de mots dans la chaine
 print("Le nombre de mots dans la chaîne est : \n", nb_de_mots)
 
